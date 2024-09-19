1d0002c96ff6fd5193991f0dccd610c97ff15a06
Praveen Goswami <praveen828589@gmail.com>
2024-09-19 19:25:13 +0530

    Fix responsiveness of Open Positions Page

--- src/app/about-us/career/our-openings/page.tsx
+++ src/app/about-us/career/our-openings/page.tsx
@@ -57,7 +57,7 @@ const OpenOpenings = () => {
     <div className="page">
       {/* Hero Section */}
       <div className="w-full">
-        <section className="w-full max-w-[1440px] mx-auto px-2 sm:px-5 lg:px-10 xl:px-14 2xl:px-24">
+        <section className="website-container px-2 sm:px-5 lg:px-10 xl:px-14 2xl:px-24">
           <div className="flex justify-center">
             <BreadCrumbs />
           </div>
@@ -66,21 +66,21 @@ const OpenOpenings = () => {
 
       {/* Open Positions Section */}
       <div className="w-full border-b border-[#D9D9D9]">
-        <section className="w-full max-w-[1440px] mx-auto pt-5 pb-12 md:pb-16 xl:pb-24">
+        <section className="website-container pt-5 pb-12 md:pb-16 xl:pb-24">
           <div className="flex flex-col items-center">
             <h1 className="font-sintony text-[2.4rem] lg:text-[3.25rem] leading-[3rem] lg:leading-[4.88rem] font-bold text-center">Open Positions</h1>
-            <p className="mt-4 text-[1.125rem] leading-[1.69rem] text-center text-[#535353]">
+            <p className="mt-4 paragraph text-center">
               Apply now, be a part of Our Super Team
             </p>
 
             <JobFilter />
 
-            <div className="mt-10 w-full flex flex-col gap-5">
+            <div className="mt-10 w-full flex flex-col gap-5 section-padding-x">
               {openPositions.map((item) => {
                 return (
                   <div
                     key={item.id}
-                    className="w-full max-w-[60rem] mx-auto p-10 bg-[#FAFAFA] flex items-center justify-between"
+                    className="w-full max-w-[63rem] mx-auto p-5 md:p-10 bg-[#FAFAFA] flex flex-col gap-5 md:flex-row md:items-center md:justify-between"
                   >
                     <div>
                       <h4 className="font-sintony font-bold text-2xl leading-[2.25rem] text-[#1A1A1A]">
--- src/components/AboutUs/JobFilter.tsx
+++ src/components/AboutUs/JobFilter.tsx
@@ -9,24 +9,29 @@ import {
 } from "@/components/ui/select";
 
 interface Option {
-    value: string;
-    label: string;
+  value: string;
+  label: string;
 }
 
 interface SelectComponentProps {
-    placeholder: string;
-    options: Option[];
+  placeholder: string;
+  options: Option[];
 }
 
-const SelectComponent: React.FC<SelectComponentProps> = ({ placeholder, options }) => {
+const SelectComponent: React.FC<SelectComponentProps> = ({
+  placeholder,
+  options,
+}) => {
   return (
     <Select>
-      <SelectTrigger className="w-full">
+      <SelectTrigger className="w-full max-w-[20rem]">
         <SelectValue placeholder={placeholder} />
       </SelectTrigger>
       <SelectContent>
         {options.map((option) => (
-          <SelectItem key={option.label} value={option.value}>{option.label}</SelectItem>
+          <SelectItem key={option.label} value={option.value}>
+            {option.label}
+          </SelectItem>
         ))}
       </SelectContent>
     </Select>
@@ -35,29 +40,31 @@ const SelectComponent: React.FC<SelectComponentProps> = ({ placeholder, options 
 
 const JobFilter = () => {
   return (
-    <div className="mt-14 flex items-center gap-5 w-full max-w-[60rem]">
-        <SelectComponent 
-            placeholder="All Location"
-            options={[
-                { value: "delhi", label: "Delhi" },
-                { value: "mumbai", label: "Mumbai" },
-                { value: "bangalore", label: "Bangalore" },
-            ]}
+    <div className="mt-7 xl:mt-14 section-padding-x w-full">
+      <div className="flex flex-wrap justify-center items-center gap-3 md:gap-5 w-full max-w-[63rem] mx-auto">
+        <SelectComponent
+          placeholder="All Location"
+          options={[
+            { value: "delhi", label: "Delhi" },
+            { value: "mumbai", label: "Mumbai" },
+            { value: "bangalore", label: "Bangalore" },
+          ]}
         />
-        <SelectComponent 
-            placeholder="Work Type"
-            options={[
-                { value: "onsite", label: "Onsite" },
-                { value: "remote", label: "Remote" },
-            ]}
+        <SelectComponent
+          placeholder="Work Type"
+          options={[
+            { value: "onsite", label: "Onsite" },
+            { value: "remote", label: "Remote" },
+          ]}
         />
-        <SelectComponent 
-            placeholder="Department"
-            options={[
-                { value: "marketing", label: "Marketing" },
-                { value: "operation", label: "Operation" },
-            ]}
+        <SelectComponent
+          placeholder="Department"
+          options={[
+            { value: "marketing", label: "Marketing" },
+            { value: "operation", label: "Operation" },
+          ]}
         />
+      </div>
     </div>
   );
 };
